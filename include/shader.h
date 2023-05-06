d23ca91b3eff4ddd6be1c38a0d1b6bdeff2a156f
CLI commit <kevinncosby2@gmail.com>
2023-05-06 07:58:27 -0400

    removed old files

--- include/shader.h
+++
@@ -1,6 +0,0 @@
-#ifndef SHADER_H
-#define SHADER_H
-
-unsigned int loadShader(GLenum type, const std::string& filename);
-
-#endif
--- include/test.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_H
-#define TEST_H
-
-#include <string>
-
-std::string test_str = "build system works!";
-std::string test2_str = "test build system works!";
-
-#endif
